017a73f52d2897ab003adf3ff85c1e8f796a061d
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-05-02 16:03:21 +0200

    Limpieza archivos innecesarios
    Co-Authored-By: Sofía Martínez Parada <55553291+sofiamartinezparada@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-# Inicializa la aplicación
+# Imports
 import os
 import sys
 from PyQt5 import QtMultimedia, QtWidgets, uic
 
+# Variables globales
+RANKING_DEFAULT = 5
 
 class MyWindow(QtWidgets.QMainWindow):
+    
     def __init__(self):
         super(MyWindow,self).__init__()
 
         #Iniciamos la primera ventana
         uic.loadUi('main.ui',self)      
-        """qtRectangle = self.frameGeometry()
-        centerPoint = QtWidgets.QDesktopWidget().availableGeometry().center()
-        qtRectangle.moveCenter(centerPoint)
-        self.move(qtRectangle.topLeft())"""
-
 
         #Boton recomendar
         self.btnRecomendar.clicked.connect(self.recomendar)
@@ -22,36 +20,15 @@ class MyWindow(QtWidgets.QMainWindow):
         ## Botón predecir
         self.btnPredecir.clicked.connect(self.predecir)
         
-    # Abrir cuadro de diálogo
-    def openDialogBox(self):
-        filename = QtWidgets.QFileDialog.getOpenFileName()
-        return filename
     
     def recomendar(self):
         pass
     
+    
     def predecir(self):
         pass
 
-    #------------ Funciones Botones Segunda Ventana ------------#
-    """
-    #Cambiar a segunda pantalla
-    def cambiarSegundaVentana(self):
-        #Cargamos la segunda ventana
-        uic.loadUi('segundapantalla.ui', self)
-        qtRectangle = self.frameGeometry()
-        centerPoint = QtWidgets.QDesktopWidget().availableGeometry().center()
-        qtRectangle.moveCenter(centerPoint)
-        self.move(qtRectangle.topLeft())
-    """
-    
-    #Guarda los cambios en un txt
-    def guardarCambios(self):
-        global texto
-        texto = self.txt_transcripcion.toPlainText()
-        filename = self.txt_filename.toPlainText()
-        save(texto, filename)
-
+  
 #Método main de la aplicación
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
