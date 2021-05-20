4f00da391a2d9d68e2a9adcfd56050aac6f5960a
sofiamartinezparada <55553291+sofiamartinezparada@users.noreply.github.com>
2021-05-20 17:17:17 +0200

    por si la lio :)

--- Python/funciones.py
+++ Python/funciones.py
@@ -24,15 +24,13 @@ cursor.execute('SELECT * FROM movies')
 result = cursor.fetchall()
 movies = pd.DataFrame.from_records(result, exclude = ['genres'], columns = ['movieId' , 'title', 'genres'])
 movie_to_idx = { movie: i for i, movie in enumerate(list(movies.set_index('movieId').loc[df_movie_features.index].title))}
-
 connection.close()
+
 # Función del coseno ajustado
 """
 SIM(A,B) = sumatorio{ (ru,a - media(ru)) * (ru,b - media(ru)) } / sqrt{ sumatorio { [(ru,a - media(ru)]^2 } } * sqrt{ sumatorio { [(ru,b - media(ru)]^2 } }
 Resultados entre [-1,1]
 """
-
-# TODO: mezclar absa y absa2 para conseguir el código final
 
 
 #funcion que devuelve la matriz de pesos (ajustada item-item)
@@ -60,7 +58,6 @@ def fuzzy(movie_selected, verbose = True):
             #Se mira si se parece y si es asi se guarda
             if parecido >= 65:
                 connection = sqlite3.connect(r'Database/Movielens.db')
-
                 cursor = connection.cursor()
                 sql = 'SELECT * FROM movies where title = \'' + title + '\''
                 cursor.execute(sql)
@@ -78,33 +75,7 @@ def fuzzy(movie_selected, verbose = True):
     except:
         return None
 
-def cosine_sim(df1, df2):
-    
-    df1na = df1.isna()
-    df1clean = df1[~df1na]
-    df2clean = df2[~df1na]
-
-    df2na = df2clean.isna()
-    df1clean = df1clean[~df2na]
-    df2clean = df2clean[~df2na]
-
-
-    # Compute cosine similarity
-    distance = cosine(df1clean, df2clean)
-    sim = 1 - distance
-
-    return sim
-
 def cosine_similarity(adj,mov1,mov2):
-    '''
-    a = adj.loc[adj['movieId'] == mov1, 'rating_adjusted']
-    b = adj.loc[adj['movieId'] == mov2, 'rating_adjusted']
-    frame = { 'a': a, 'b': b }
-    result = pd.DataFrame(frame).fillna(np.nan)
-    ajustada = result.dropna(how = 'any', axis = 'rows')
-    print(ajustada)
-    scoreDistance = cosine(ajustada['a'], ajustada['b'])
-    return scoreDistance'''
     para_coseno = pd.concat([adj[mov1], adj[mov2]], axis=1, keys=[mov1, mov2])
     
     ajustada = para_coseno.dropna(how = 'any', axis = 'rows')
@@ -112,7 +83,7 @@ def cosine_similarity(adj,mov1,mov2):
         scoreDistance = 0
     else:
         scoreDistance = cosine(ajustada[mov1], ajustada[mov2])
-    print(str(mov2) + '------' + str(scoreDistance))
+  
     return scoreDistance
 
 def consultarBBDD(userId, movieId):
@@ -145,18 +116,10 @@ def prediccion(movieTitle, userId):
             for valorada in valoradas:
                 distancia = cosine_similarity(ajustada, movieId, valorada)
                 if (distancia != 0):
-                    #distancia = cosine_sim(ajustada[movieId], ajustada[valorada])
-                    #sin ajustar!
                     scoreB = consultarBBDD(userId, valorada)
-                    print('Score: ', scoreB)
-                    '''
-                    rate_valorada = ratings.iloc[valorada]
-                    scoreB = rate_valorada['rating']'''
                     numerador  = distancia * scoreB
                     sumatorioenumerador = sumatorioenumerador + numerador
                     sumatoriodenominador = sumatoriodenominador + distancia
-                print(valorada)
-                print('*****************************')
             pred = sumatorioenumerador / sumatoriodenominador
             return pred
         else:
@@ -190,15 +153,5 @@ def download_image(movieId):
         print('Fallo en la conexion a la BBDD')
 
 
-
 def recomendacion():
     pass
-#pred = prediccion('toy estory', str(1))
-# si pred = 1e-8 el señoro ya la valoro
-
-#peli = fuzzy(movie_to_idx,'toy estory')
-
-#Para acceder al nombre peli[0][0]
-#Para el movieId peli[0][1]
-#print(peli[0][0])
-#print(peli[0][1])
